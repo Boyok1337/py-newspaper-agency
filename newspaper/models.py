e4018d59d2322d307c23f57ff3999c2d922320f1
Andrii <andreboiko1@gmail.com>
2024-05-06 09:54:47 +0300

    make migrations

--- newspaper/models.py
+++ newspaper/models.py
@@ -4,9 +4,25 @@ from django.db import models
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(null=True, blank=True)
+    groups = models.ManyToManyField(
+        "auth.Group",
+        verbose_name="groups",
+        blank=True,
+        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+        related_name="redactor_set",
+        related_query_name="redactor",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        verbose_name="user permissions",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        related_name="redactor_set",
+        related_query_name="redactor",
+    )
 
     class Meta:
-        ordering = ("username", "years_of_experience")
+        ordering = ("username", "-years_of_experience")
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
